a326e6609319b3af6c0831a925552e3526b73df2
pavan <pavandotnet007@outlook.com>
2022-12-10 22:50:50 +0530

    navbar updated

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import { FiLogOut, FiLogIn } from "react-icons/fi";
+import { useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import { FiLogOut, FiLogIn } from 'react-icons/fi';
 // Redux
-import { useAppSelector, useAppDispatch } from "../../hooks/typedReduxHooks";
-import { logOut, getUserWithSessionId } from "../../app/features/authSlice";
-import { setProgressBar } from "../../app/features/uiSlice";
+import { useAppSelector, useAppDispatch } from '../../hooks/typedReduxHooks';
+import { logOut, getUserWithSessionId } from '../../app/features/authSlice';
+import { setProgressBar } from '../../app/features/uiSlice';
 
 // Components
-import ProgressLine from "../ui/loader/ProgressLine";
+import ProgressLine from '../ui/loader/ProgressLine';
 
 // CSS
-import "react-toastify/dist/ReactToastify.css";
-import "./Navbar.css";
+import 'react-toastify/dist/ReactToastify.css';
+import './Navbar.css';
 
 const Navbar = () => {
 	const dispatch = useAppDispatch();
@@ -29,8 +29,8 @@ const Navbar = () => {
 
 	useEffect(() => {
 		// If user is authenticated redirect to home page
-		if (location.pathname === "/login" && isAuthenticated) {
-			navigate("/");
+		if (location.pathname === '/login' && isAuthenticated) {
+			navigate('/');
 		}
 		// Refetch user data when browser is refreshed
 		if (sessionId && !user) {
@@ -54,25 +54,25 @@ const Navbar = () => {
 
 	return (
 		<div className='fixed z-50 w-full top-0'>
-			<section className='bg-primary text-white flex justify-between px-10 items-center h-16  ' id='navbarId'>
+			<section className='bg-primary text-white flex justify-between px-3 sm:px-10 items-center h-16  ' id='navbarId'>
 				<div className='left flex space-x-10 items-center'>
 					<Link to='/'>
 						<h1 className='logo pointer text-3xl font-bold text-style  '>MDB</h1>
 					</Link>
 				</div>
-				<div className='right flex sm:space-x-10 items-center text-lg '>
+				<div className='right flex sm:space-x-10 items-center sm:text-lg space-x-3 '>
 					{isAuthenticated ? (
 						<>
-							<Link to='/watchlist' className='hidden sm:block'>
+							<Link to='/watchlist' className='sm:block'>
 								<h1 className='pointer font-semibold'>WatchList</h1>
 							</Link>
-							<Link to='/favorites' className='hidden sm:block'>
+							<Link to='/favorites' className='sm:block'>
 								<h1 className='pointer font-semibold'>Favorites</h1>
 							</Link>
 							<div onClick={handelLogout} className='cursor-pointer'>
 								<div className='pointer font-semibold flex justify-center items-center space-x-1'>
 									<span>Logout</span>
-									<FiLogOut />{" "}
+									<FiLogOut />{' '}
 								</div>
 							</div>
 						</>
@@ -91,12 +91,24 @@ const Navbar = () => {
 					visualParts={[
 						{
 							percentage: `${percentage}%`,
-							color: "#2DBBD0",
+							color: '#2DBBD0',
 						},
 					]}
 				/>
 			)}
-			{error && <ToastContainer position='top-right' autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />}
+			{error && (
+				<ToastContainer
+					position='top-right'
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
+			)}
 		</div>
 	);
 };
